138a6a15ae1ea6dd9781339453d378a94c068974
Bazuk1 <64947923+Bazuk1@users.noreply.github.com>
2020-09-06 11:03:37 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+
+var i = 0;
+var textsize = 64;
+var color1 = 5;
+var color2 = 5;
+var color3 = 5;
+var text = document.getElementById("text");
+var j = 0;
+setInterval(() => {
+    if (i < 16) {
+        textsize = 8 + textsize;
+        document.getElementById("text").style.fontSize = textsize;
+        i++;
+    }
+    if (i == 16) {i = 32}
+    if (i > 16) {
+        textsize = 64;
+        textsize = 8 - textsize;
+        document.getElementById("text").style.fontSize = textsize;
+        i = 1 - i;
+    }
+}, 30);
+
+setInterval(() => {
+    document.getElementById("text").style.color = "rgb("+color1+", "+color2+", "+color3+")";
+
+    if (j < 5) {color1 = color1 + 20; color2 = color2 + 15; color3 = color3 + 30; j++}
+    if (j > 5) {color1 = color1 - 30; color2 = color2 - 35; color3 = color3 - 20; j--}
+    if (j == 10) {j = 0;}
+    if (j == 5) {j = 0;}
+    if (color1 > 255) {color1 = 5}
+    if (color2 > 255) {color2 = 5}
+    if (color3 > 255) {color3 =5}
+
+}, 60);
+
+
+
+
